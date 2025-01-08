a004124603be43d55643d55b8be3c07fd478c981
jalal <jalaldjigbenou0@gmail.com>
2025-01-08 09:16:51 +0100

    Création d'un modèle user personnalisé.

--- fotoblog/settings.py
+++ fotoblog/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'fotoblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog_db',
+        'USER': 'fotoblog',
+        'PASSWORD': 'blog',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -124,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'authentication.User'
